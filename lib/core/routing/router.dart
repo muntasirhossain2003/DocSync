5ada3f925b3dbfd2f22c493d90d0d686c15492fe
rafidahmed816 <rafidahmed816@gmail.com>
2025-10-13 07:46:04 +0600

    auth done

---
+++ lib/core/routing/router.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import '../../features/auth /presentation/pages/log_in.dart';
+import '../../features/auth /presentation/pages/register.dart';
+import '../../features/auth /presentation/provider/auth_provider.dart';
+import '../../shared/widgets/splash_screen.dart';
+
+
+final appRouterProvider = Provider<GoRouter>((ref) {
+  final authState = ref.watch(authStateProvider);
+
+  return GoRouter(
+    initialLocation: '/splash',
+    routes: [
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginPage(),
+      ),
+      GoRoute(
+        path: '/register',
+        builder: (context, state) => const RegisterPage(),
+      ),
+      GoRoute(
+        path: '/splash',
+        builder: (context, state) => const SplashScreen(),
+      ),
+    ],
+  );
+});
